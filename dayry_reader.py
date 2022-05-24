d80782ebf19f84618870572ce5797279af1cabfd
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2022-05-24 08:35:58 +0300

    Update dayry_reader.py
    добавил код.  выполнены задания.

--- dayry_reader.py
+++ dayry_reader.py
@@ -17,3 +17,22 @@ file_diary = open("dayry.txt")
 diary_text = file_diary.read()
 file_diary.close()
 split_diary = diary_text.split('___')
+
+# Вывод случайного задания и случайной записи дневника
+import random
+
+file = open("ITDO.txt")
+text = file.read()
+file.close()
+split_text = text.split('><')
+
+file_diary = open("dayry.txt")
+diary_text = file_diary.read()
+file_diary.close()
+split_diary = diary_text.split('___')
+
+number = random.randint(0, len(split_text))
+print(split_text[number])
+
+number_note = random.randint(0, len(split_diary))
+print(split_diary[number_note])
